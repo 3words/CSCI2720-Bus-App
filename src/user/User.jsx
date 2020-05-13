aab48e694ac02eb28c6fefe7e76ea8c9f9adf69f
Alex Ho <alexho225@yahoo.com.hk>
2020-05-13 15:33:50 +0800

    add user.css

--- src/user/User.jsx
+++ src/user/User.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './User.css';
 
 class User extends React.Component {
 
   render() {
     return (
       <div className='User-interface'>
-        <h1> Hello, {this.props.user} </h1>
-        <button className="btn btn-primary btn-block" onClick={this.props.logout}>Logout</button>
+        <div className="header">
+          <button className="logout-button btn btn-primary" onClick={this.props.logout}>Logout</button>
+          <span className="userName">Hello, {this.props.user}</span>
+        </div>
       </div>
     );
   }
