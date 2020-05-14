1b635dcf0828f8d43118aacc072a3c8bb8a1f095
Henryhin <aab11341@gmail.com>
2020-05-14 23:53:21 +0800

    edit user interface

--- src/user/User.jsx
+++ src/user/User.jsx
@@ -1,7 +1,220 @@
 import React from 'react';
+import axios from 'axios';
 import './User.css';
+let allRoutes =[967,969]
+
+
+class Content extends React.Component {
+
+  
+
+  render() {
+    return (
+      <tbody>
+        {this.props.locationList}
+      </tbody>
+        
+    );
+  }
+}
+
+class ListLocation extends React.Component {
+  sortTableAlpha = (n) => {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table");
+    switching = true;
+    dir = "asc"; 
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++;      
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+  sortTableNum = (n) => {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table");
+    switching = true;
+    dir = "asc"; 
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++;      
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+  searchFunction = (inputKeyword,idx) => {
+    var filter, table, tr, td, i, txtValue;
+    filter = inputKeyword.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[idx];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+  handleContextClick = (event) =>{
+    event.preventDefault();
+    let deleteConfirm = window.confirm("Do you want to search for locations by "+event.target.innerHTML+" field?")
+    if(!deleteConfirm) return false;
+
+    let inputKeyword = window.prompt("Enter keyword in "+event.target.innerHTML+" field");
+
+    if(!inputKeyword) return false;
+
+    var idx;
+    switch (event.target.innerHTML) {
+      case "LocID":
+        idx = 0;
+        break;
+      case "Name":
+        idx = 1;
+        break;
+      case "Longitude":
+        idx = 2;
+        break;
+      case "Latitude":
+        idx = 3;
+        break;
+      case "Route":
+        idx = 4;
+        break;
+      case "Dest":
+        idx = 5;
+        break;
+      case  "Orig":
+        idx = 6;
+      case  "ETA":
+        idx = 7;
+        break;
+    }
+    this.searchFunction(inputKeyword,idx)
+  }
+
+
+  render() {
+    return (
+
+      <table id='table' className="table table-bordered table-hover table-condensed">
+      <thead><tr>
+      <th title="Field #1" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableNum(0)}>LocID</th>
+      <th title="Field #2" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableAlpha(1)}>Name</th>
+      <th title="Field #3" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableNum(2)}>Longitude</th>
+      <th title="Field #4" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableNum(3)}>Latitude</th>
+      <th title="Field #5" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableNum(4)}>Route</th>
+      <th title="Field #6" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableAlpha(5)}>Dest</th>
+      <th title="Field #7" onContextMenu={this.handleContextClick} onClick={()=>this.sortTableAlpha(6)}>Orig</th>
+      <th title="Field #8" onContextMenu={this.handleContextClick} onClick="">ETA</th>
+      </tr></thead>
+      <Content locationList={this.props.locationList}></Content>
+    </table>
+  
+    
+    );
+  }
+}
+
 
 class User extends React.Component {
+
+
+  handleListLocations = async (addStateList) => {
+   // alert(allRoutes.length)
+   var eachRoute=[];
+    for (var i =0;i<allRoutes.length;i++){
+      await axios.get('/list',{
+        params:{
+          route : allRoutes[i]
+        }
+      })
+      .then(function(res) {
+        var obj = JSON.parse(JSON.stringify(res.data))
+        //alert(obj.data[0]);
+    
+        
+        for (var i in obj.data){
+          const row = (
+              <tr>
+              <td>{obj.data[i].locID}</td>
+              <td>{obj.data[i].name}</td>
+              <td>{obj.data[i].long}</td>
+              <td>{obj.data[i].lat}</td>
+              <td>{obj.data[i].route}</td>
+              <td>{obj.data[i].dest}</td>
+              <td>{obj.data[i].orig}</td>
+              <td>{obj.data[i].eta}</td>
+              </tr>
+          );
+          eachRoute.push(row)
+        }
+         
+       //addStateList(eachRoute);
+      
+     
+      });
+    }
+    addStateList(eachRoute);
+  };
+
+
 
   render() {
     return (
@@ -10,6 +223,12 @@ class User extends React.Component {
           <button className="logout-button btn btn-primary" onClick={this.props.logout}>Logout</button>
           <span className="userName">Hello, {this.props.user}</span>
         </div>
+
+        <button className="btn btn-primary" onClick={()=>{this.props.listloc()}}>List all Location</button>
+        <button className="btn btn-primary" onClick={()=>{this.handleListLocations(this.props.changeLocationList)}}>Load List</button>
+        {this.props.showList &&
+          <ListLocation locationList={this.props.locationList}></ListLocation>
+        }
       </div>
     );
   }
