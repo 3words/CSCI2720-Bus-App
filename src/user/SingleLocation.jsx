0273343265fb1e9f5a2e3f944decb7fdce2d4f9f
Alex Ho <alexho225@yahoo.com.hk>
2020-05-15 08:30:26 +0800

    update singlelocation.jsx

--- src/user/SingleLocation.jsx
+++ src/user/SingleLocation.jsx
@@ -7,7 +7,7 @@ class SingleLocation extends React.Component {
   render() {
     return (
       <div className="single-location">
-
+        <h5>Location Name: {this.props.relatedStop[0].loc.name}</h5>
       </div>
     );
   }
